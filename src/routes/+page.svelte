566a72ce8e558e2c59332b2162f265c22c115079
binilbp <biniloffcl@gmail.com>
2025-11-01 11:29:13 +0530

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Header from './header.svelte';
+	import Body from './body.svelte';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-start bg-blue-300">
+	<Header />
+	<Body />
+</div>
---
+++ src/routes/body.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-start p-5">
+	<img class="h-auto w-3/4" src="src/lib/assets/VCompass/plaincompassbg.png" alt="compass" />
+	<button class="h-1 border-blue-200"> Start Tracking </button>
+</div>
