ae99fb8fe4a319b1a61fb579fa189c63f51456f2
binilbp <biniloffcl@gmail.com>
2025-11-01 17:36:29 +0530

    Add visual info on current process

--- src/lib/body.svelte
+++ src/lib/body.svelte
@@ -1,7 +1,11 @@
 <script>
 	let angle = $state(0);
 	let isSpinning = $state(false);
+	let trackingPlane = $state(false);
+	let gettingLocation = $state(0);
 	let planefound = false;
+	let currentLat = null;
+	let currentLong = null;
 
 	function rotate() {
 		angle += 1;
@@ -9,6 +13,32 @@
 
 	function trackplane() {
 		isSpinning = !isSpinning;
+		trackingPlane = !trackingPlane;
+		navigator.geolocation.getCurrentPosition(setPosition, locationError);
+	}
+
+	function setPosition(position) {
+		currentLat = position.coords.latitude;
+		currentLong = position.coords.longitude;
+		console.log('Got the location:', currentLat, currentLong);
+		gettingLocation = 1;
+	}
+
+	function locationError(err) {
+		gettingLocation = -1;
+		switch (err) {
+			case err.PERMISSION_DENIED:
+				console.log('User denied the request for Geolocation.');
+				break;
+			case err.POSITION_UNAVAILABLE:
+				console.log('Location information is unavailable.');
+				break;
+			case err.TIMEOUT:
+				console.log('The request to get user location timed out.');
+				break;
+			default:
+				console.log('An unknown error occurred.');
+		}
 	}
 </script>
 
@@ -27,11 +57,20 @@
 			alt="white dials of compass"
 		/>
 	</div>
-	<!-- <img class="h-auto w-3/4" src="src/lib/assets/VCompass/dial_white.png" alt="compass" /> -->
 	<button
 		class="rounded-2xl bg-sky-900 px-4 py-3.5 font-boogaloo text-lime-500 shadow-md"
 		onclick={trackplane}
 	>
 		Be Happy!
 	</button>
+
+	{#if trackingPlane && gettingLocation === 0}
+		<p class="font-boogaloo text-blue-50">Getting your location</p>
+	{:else if trackingPlane && gettingLocation === 1}
+		<p class="text-center font-boogaloo text-blue-50">
+			Got your location<br />Searching the sky near you!
+		</p>
+	{:else if gettingLocation === -1}
+		<p class="font-boogaloo text-blue-50">Did not get your location !</p>
+	{/if}
 </div>
