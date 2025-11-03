85f8b117c182b2203e552e5b3387a6241564073c
binil <biniloffcl@gmail.com>
2025-11-03 21:37:26 +0530

    Start getting location

--- src/lib/components/Body.svelte
+++ src/lib/components/Body.svelte
@@ -19,7 +19,7 @@
 	let isSpinning: boolean = $derived(isTrackingPlane);
 	//white plane angle control variable
 	let planeAngle: number = $state(0);
-	let statusCode: number = $state(119);
+	let statusCode: number = $state(0);
 
 	function rotatePlane(angle: number) {
 		//this function set the angle of the white plane
@@ -30,21 +30,26 @@
 		//primary task is to get location
 		statusCode = 119;
 		try {
-			console.log('Getting location');
 			const location = await getPositionCords();
 			const userLatitude = location.latitude;
 			const userLongitude = location.latitude;
 			console.log('Got user location :', userLatitude, userLongitude);
-		} catch {}
+		} catch (error: any) {
+			console.error('Error retrieving location:', error.message);
+			statusCode = 120;
+			isTrackingPlane = false;
+		}
 	}
 	function trackPlane() {
 		isTrackingPlane = !isTrackingPlane;
+		//if illel off cheyumbolum on cheyumbolum startEngine run aavum
+		if (isTrackingPlane) {
+			startEngine();
+		}
 	}
 </script>
 
-<div
-	class="flex flex-col items-center justify-start bg-fuchsia-500 md:flex-row md:items-start md:justify-around"
->
+<div class="flex flex-col items-center justify-start md:flex-row md:items-start md:justify-around">
 	<div class="flex flex-col items-center justify-start gap-8 p-5 md:flex-row md:justify-between">
 		<div class="grid place-items-center">
 			<img
@@ -68,7 +73,7 @@
 			Ready
 		</button>
 	</div>
-	<div class="flex flex-col items-center justify-start bg-amber-500 px-5 py-2 md:px-24 md:pt-30">
+	<div class="flex flex-col items-center justify-start px-5 py-2 md:px-24 md:pt-30">
 		<InfoBar {statusCode} />
 	</div>
 </div>
--- src/lib/components/InfoBar.svelte
+++ src/lib/components/InfoBar.svelte
@@ -4,13 +4,24 @@
 		mal: string;
 	}
 	let { statusCode } = $props();
-	console.log('prop:', statusCode);
 
 	function setInfo(statusCode: number): infoType {
 		switch (statusCode) {
+			case 0:
+				return {
+					eng: 'Press Ready when u ready!!',
+					mal: 'ഒന്നും നോക്കണ്ട നേരെ റെഡി അമർത്തിക്കോ<br/>ഇതൊക്കെ ഒരു വിശ്വാസല്ലേ !'
+				};
 			case 119:
-				console.log('got status 119');
+				console.log('Getting location');
 				return { eng: 'Getting your position', mal: 'ഇത് എവിടെയാണ്...' };
+
+			case 120:
+				console.log('Did not get location');
+				return {
+					eng: "Couldn't get you position.<br/>Maybe reload and try again?.",
+					mal: 'കിട്ടിയില്ലലോ, ലൊക്കേഷൻ ഒക്കെ ഓൺ അല്ലെ?'
+				};
 			default:
 				return { eng: '', mal: '' };
 		}
@@ -20,5 +31,8 @@
 	const info: infoType = $derived(setInfo(statusCode));
 </script>
 
-<p class="font-comfortaa text-sm font-semibold text-sky-50 md:text-lg">{info.eng}</p>
-<p class="font-baloo text-sm font-semibold text-sky-50 md:text-lg">{info.mal}</p>
+<!-- @html tag identified the html tag in the string and render it as html -->
+<p class=" text-center font-comfortaa text-sm font-semibold text-sky-50 md:text-lg">
+	{@html info.eng}
+</p>
+<p class="text-center font-baloo text-sm font-semibold text-sky-50 md:text-lg">{@html info.mal}</p>
