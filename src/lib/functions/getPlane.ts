4b1c123f6810fdc4fb673a1be7e50e77a423bc0d
binil <biniloffcl@gmail.com>
2025-11-04 15:56:53 +0530

    Set url

--- src/lib/functions/getPlane.ts
+++ src/lib/functions/getPlane.ts
@@ -1,6 +1,7 @@
 export async function getPlane(userLatitude: number, userLongitude: number) {
 	console.log(`Running getPlane`);
-	const apiUrl = `http://127.0.0.1:8000/api/planes?lat=${userLatitude}&lon=${userLongitude}`;
+	// const apiUrl = `http://127.0.0.1:8000/api/planes?lat=${userLatitude}&lon=${userLongitude}`;
+	const apiUrl = `http://vcompassapi.onrender.com/api/planes?lat=${userLatitude}&lon=${userLongitude}`;
 
 	const response = await fetch(apiUrl);
 	if (!response.ok) {
