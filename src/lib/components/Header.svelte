fd83c445bcb067f5480439312ac63d6d97e2f7a3
binil <biniloffcl@gmail.com>
2025-11-04 23:32:43 +0530

    Add subheading and fix ui

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,11 +3,14 @@
 	let malayalamHeader: string = 'വിമാനം കോമ്പസ് ';
 </script>
 
-<div class="p-5">
+<div class="pt-2">
 	<h1 class="pt-4 text-center font-boogaloo text-5xl font-semibold text-sky-900">
 		{englishHeader}
 	</h1>
 	<h2 class="pt-2 text-center font-chilanka text-4xl font-semibold text-sky-900">
 		{malayalamHeader}
 	</h2>
+	<p class="pb-5 text-center font-comfortaa text-xs text-sky-900">
+		RealTime Flight Direction Above you
+	</p>
 </div>
