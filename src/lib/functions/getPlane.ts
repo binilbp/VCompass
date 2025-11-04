ca743d072ad49984c64a4bf72c5a67cf75f621e8
binil <biniloffcl@gmail.com>
2025-11-04 16:06:16 +0530

    Fix connection issues

--- src/lib/functions/getPlane.ts
+++ src/lib/functions/getPlane.ts
@@ -1,7 +1,7 @@
 export async function getPlane(userLatitude: number, userLongitude: number) {
 	console.log(`Running getPlane`);
 	// const apiUrl = `http://127.0.0.1:8000/api/planes?lat=${userLatitude}&lon=${userLongitude}`;
-	const apiUrl = `http://vcompassapi.onrender.com/api/planes?lat=${userLatitude}&lon=${userLongitude}`;
+	const apiUrl = `https://vcompassapi.onrender.com/api/planes?lat=${userLatitude}&lon=${userLongitude}`;
 
 	const response = await fetch(apiUrl);
 	if (!response.ok) {
